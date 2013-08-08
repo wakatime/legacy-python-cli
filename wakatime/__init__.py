a23ef6cbc03a4cd92178db79606fe894030f01de
Alan Hamlett <alan.hamlett@gmail.com>
2013-08-08 02:11:46 -0700

    v0.3.0

--- wakatime/__init__.py
+++ wakatime/__init__.py
@@ -12,7 +12,7 @@
 from __future__ import print_function
 
 __title__ = 'wakatime'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __author__ = 'Alan Hamlett'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2013 Alan Hamlett'
