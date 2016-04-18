138702f9921f16b95c887deec336432a6a2a1dfc
Alan Hamlett <alan.hamlett@gmail.com>
2016-04-18 20:40:02 +0200

    add support python versions

--- setup.py
+++ setup.py
@@ -40,5 +40,11 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Topic :: Text Editors',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ),
 )
